ea0e248e00d16edcdc4e553a4d40f0242c17acc5
ZX <903464207@qq.com>
2023-09-15 14:35:17 +0800

    优化代码

--- goravel/routes/api.go
+++ goravel/routes/api.go
@@ -12,32 +12,43 @@ func Api() {
 	//无权限
 	facades.Route().Prefix("api/admin").Middleware(middleware.AdminCheck(), middleware.Recovery()).Group(func(router route.Router) {
 
-		router.Get("login", admin.NewIndexController().Login)           //登录请求
-		router.Get("uploadPic", admin.NewIndexController().UploadPic)   //上传图片文件
-		router.Get("uploadFile", admin.NewIndexController().UploadFile) //上传普通文件
-
-		router.Get("/admin/getList", admin.NewAdminController().GetList)
-		router.Get("/admin/getAll", admin.NewAdminController().GetAll)
-		router.Get("/admin/add", admin.NewAdminController().Add)
-		router.Get("/admin/save", admin.NewAdminController().Save)
-		router.Get("/admin/delete", admin.NewAdminController().Delete)
-
+		router.Post("login", admin.NewIndexController().Login)           //登录请求
+		router.Post("uploadPic", admin.NewIndexController().UploadPic)   //上传图片文件
+		router.Post("uploadFile", admin.NewIndexController().UploadFile) //上传普通文件
 	})
 
 	//有权限检查
 	facades.Route().Prefix("admin").Middleware(middleware.AdminCheck(), middleware.Recovery()).Group(func(router route.Router) {
-		//router::post('/getMenu', [app\controller\Admin\IndexController::class, 'getMenu'])->name('获取菜单信息'); //获取菜单信息
-		//Route::post('/getInfo', [app\controller\Admin\IndexController::class, 'getInfo']); //获取用户信息
-		//Route::post('/logout', [app\controller\Admin\IndexController::class, 'logout']); //登出
-		//Route::post('/getVersion', [app\controller\Admin\IndexController::class, 'getVersion']); //获取版本信息
-		//Route::post('/changePwd', [app\controller\Admin\IndexController::class, 'changePwd']);
+
+		//router.Post("getMenu", admin.NewIndexController().GetMenu)       //获取菜单信息
+		//router.Post("getInfo", admin.NewIndexController().GetInfo)       //获取用户信息
+		//router.Post("logout", admin.NewIndexController().logout)         //登出
+		//router.Post("getVersion", admin.NewIndexController().getVersion) //获取版本信息
+		//router.Post("changePwd", admin.NewIndexController().changePwd)   //修改密码
 		//
-		//Route::post('/getGroupTree', [app\controller\Admin\IndexController::class, 'getGroupTree']);//获取权限组树状结构
-		//Route::post('/getMenuTree', [app\controller\Admin\IndexController::class, 'getMenuTree']);//获取所有权限树状结构
-		//Route::post('/getDownloadCateTree', [app\controller\Admin\IndexController::class, 'getDownloadCateTree']);
-		//Route::post('/getNewsCateTree', [app\controller\Admin\IndexController::class, 'getNewsCateTree']);
-		//Route::post('/getProductCateTree', [app\controller\Admin\IndexController::class, 'getProductCateTree']);
-		//Route::post('/getVideoCateTree', [app\controller\Admin\IndexController::class, 'getVideoCateTree']);
-		//Route::post('/getBannerCateTree', [app\controller\Admin\IndexController::class, 'getBannerCateTree']);
+		//router.Post("getGroupTree", admin.NewIndexController().GetGroupTree)
+		//router.Post("getMenuTree", admin.NewIndexController().GetMenuTree)
+		//router.Post("getDownloadCateTree", admin.NewIndexController().GetDownloadCateTree)
+		//router.Post("getNewsCateTree", admin.NewIndexController().GetNewsCateTree)
+		//router.Post("getProductCateTree", admin.NewIndexController().GetProductCateTree)
+		//router.Post("getVideoCateTree", admin.NewIndexController().GetVideoCateTree)
+		//router.Post("getBannerCateTree", admin.NewIndexController().GetBannerCateTree)
+
+		router.Prefix("admin").Group(func(router1 route.Router) {
+			router1.Get("/getList", admin.NewAdminController().GetList)
+			router1.Get("/getAll", admin.NewAdminController().GetAll)
+			router1.Get("/add", admin.NewAdminController().Add)
+			router1.Get("/save", admin.NewAdminController().Save)
+			router1.Get("/delete", admin.NewAdminController().Delete)
+		})
+
+		router.Prefix("index").Group(func(router1 route.Router) {
+			router1.Get("/getList", admin.NewIndexController().GetList)
+			router1.Get("/getAll", admin.NewIndexController().GetAll)
+			router1.Get("/add", admin.NewIndexController().Add)
+			router1.Get("/save", admin.NewIndexController().Save)
+			router1.Get("/delete", admin.NewIndexController().Delete)
+		})
+
 	})
 }
